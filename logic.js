09404b9667892d1064674dd84980f9980f6c11a8
shshrukh <shshrukh.arbaz@gmail.com>
2025-06-20 23:50:35 +0500

    � Started modularizing renderQuestion logic (incomplete)

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 
-
+import quizQuestions from "./question.js";
 //els object will contain all required elements
 const els ={};
 
@@ -8,29 +8,36 @@ const els ={};
 const state = {
     userName : '',
     userAge : 0,
+    currentQuestionIndex : 0,
+    questions : null,
 };
 
 // dom content is loaded then init function is invoked;
 function init(){
-    casheDOM();
-    bindsEvents();
+    cacheDOM();
+    bindEvents();
 }
 
 // cashe the value of dome 
 
-function casheDOM(){
+function cacheDOM(){
     Object.assign(els,{
         userInfoScreen : document.querySelector('.userInfoScreen'),
         userInfo :       document.getElementById('userForm'),
-        inputUserNmae :       document.getElementById('userName'),
-        inputUserAge :        document.getElementById('age'),
+        inputUserName :  document.getElementById('userName'),
+        inputUserAge :   document.getElementById('age'),
         gameGuide :      document.querySelector('.quizGameGuide'),
+        nameMessage :    document.querySelector('.userNameTarget'),
+        startQuizBtn:    document.querySelector('.startQuiz'),
+        quizContainer :   document.querySelector('.quizQuestions'),
+        questionContainer:document.querySelector('.questionContainer'),
     })
 }
 
 // now make an event function 
-function bindsEvents(){
+function bindEvents(){
     els.userInfo.addEventListener('submit', handleSubmit);
+    els.startQuizBtn.addEventListener('click', handleStartQuiz)
 }
 
 // this function is call when click on submit button 
@@ -38,10 +45,10 @@ function bindsEvents(){
 function handleSubmit(e){
     e.preventDefault();
     const age = parseInt(els.inputUserAge.value);
-    const name = els.inputUserNmae.value.trim();
+    const name = els.inputUserName.value.trim();
 
     if(Number.isNaN(age) || age < 10 || 80 < age ){
-        alert('age 10 to 80 are allow to use this app');
+        alert('Only users aged between 10 and 80 are allowed to use this app.');
         return undefined;
     }
     
@@ -50,19 +57,64 @@ function handleSubmit(e){
         return undefined;
     }
 
-    const nameRegex = /^[A-Z-a-z\s]+$/;
+    const nameRegex = /^[A-Za-z\s]+$/;
     if(!nameRegex.test(name)){
-        alert('name should be contain letters and spaces');
+        alert('Name should only contain letters and spaces.');
         return undefined;
     }
     
     state.userName = name;
     state.userAge = age;
-
+    els.nameMessage.innerText = `Hello ${name}, welcome to test your knowledge!`;
     els.userInfoScreen.classList.add('hidden');
     els.gameGuide.classList.remove('hidden')
 }
 
+// on clicking start quiz handleStartQuiz start
+function handleStartQuiz(){
+    const randomTenQuestions = [...quizQuestions].sort(()=>(Math.random() - 0.5 )).slice(0,10);
+    state.questions = randomTenQuestions;
+    console.log(state);
+    els.gameGuide.classList.add('hidden');
+    renderQuestion();
+}
+
+function renderQuestion() {
+
+    const displayQuestion = els.questionContainer;
+    displayQuestion.innerHTML = '';
+
+    // add question 
+    const questionE1 = document.createElement('h2');
+    const {questions} = state;
+    console.log('our questions are', questions)
+    questions.forEach(element => {
+        console.log(element);
+        
+    });
+
+
+    els.quizContainer.classList.remove('hidden');
+    const containQuestion = els.questionContainer;
+    console.log(containQuestion);
+    
+//   container.innerHTML = ''; // clear old content
+
+//   // Add question text
+//   const questionEl = document.createElement('h2');
+//   questionEl.innerText = questionObj.question;
+//   container.appendChild(questionEl);
+
+//   // Add options
+//   questionObj.options.forEach((option, index) => {
+//     const btn = document.createElement('button');
+//     btn.innerText = option.text;
+//     btn.classList.add('optionBtn');
+//     btn.dataset.isCorrect = option.isCorrect; // for tracking
+//     btn.addEventListener('click', handleAnswer);
+//     container.appendChild(btn);
+//   });
+}
 
 
 
